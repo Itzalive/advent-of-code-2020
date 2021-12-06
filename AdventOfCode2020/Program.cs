9012a959f1745c22e0ca043ec5fa75da6e0ddad4
Pete Forrest <peter.forrest@abstractleap.com>
2021-12-06 15:10:37 +0000

    Day 6

--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -1,10 +1,10 @@
 ﻿using AdventOfCode2020;
 
-Day5.Part1(Day5.Input);
-Day5.Part2(Day5.Input);
+//Day5.Part1(Day5.Input);
+//Day5.Part2(Day5.Input);
 
-//Day6.Part1(Day6.Input);
-//Day6.Part2(Day6.Input);
+Day6.Part1(Day6.Input);
+Day6.Part2(Day6.Input);
 
 //Day7.Part1(Day7.Input);
 //Day7.Part2(Day7.Input);
